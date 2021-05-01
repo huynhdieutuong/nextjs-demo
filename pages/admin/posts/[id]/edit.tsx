a41a9c7bf42ef86fb9a457bfefc0588aaadc9542
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2021-05-01 08:53:18 +0700

    Dynamic Route

---
+++ pages/admin/posts/[id]/edit.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// /admin/posts/566/edit
+
+const EditPosts: React.FC = () => {
+  return (
+    <h1>
+      Admin - Posts Page - ID - Edit
+    </h1>
+  )
+}
+
+export default EditPosts
---
+++ pages/admin/users/[id].tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// /admin/users/20
+
+const UsersID: React.FC = () => {
+  return (
+    <h1>
+      Admin - Users Page - ID
+    </h1>
+  )
+}
+
+export default UsersID
